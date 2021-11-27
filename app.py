25d76221ff8d2df7299b1492af661490e5de285e
Kimura Taiki <s1922074@stu.musashino-u.ac.jp>
2021-11-28 00:53:59 +0900

    complete

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ import os
 import pandas as pd
 app = Flask(__name__)
 
+def split_list(l, n):
+    for idx in range(0, len(l), n):
+        yield l[idx:idx + n]
+
+def my_round(val, digit=0):
+    p = 10 ** digit
+    return (val * p * 2 + 1) // 2 / p
+
 df = pd.read_csv("data_csv_2.csv")
 # ラベル (時間)
 data_list = list(df["time"])
@@ -15,7 +23,7 @@ min_dict = {}
 all_dict = {}
 value_dict = {}
 for i in day_list:
-    mean_dict[i] = sum(list(df[i])) / len(list(df[i]))
+    mean_dict[i] = my_round(sum(list(df[i])) / len(list(df[i])),1)
     max_dict[i] = max(list(df[i]))
     min_dict[i] = min(list(df[i]))
     all_dict[i] = sum(list(df[i]))
@@ -30,8 +38,26 @@ for i in day_list:
         count += 1
 
 peak_dict = {}
+day_grow_dict = {}
+grow_list_dict = {}
+time_txt_ = ["00:00~04:00","05:00~09:00","10:00~14:00","15:00~19:00","20:00~23:00",]
 for i in day_list:
     upper_75 = int(df[i].describe()[6])
+    lower_25 = int(df[i].describe()[4])
+    count = 0
+    day_grow_dict[i] = {}
+    grow_list_dict[i] = []
+    for k in list(split_list(value_dict[i], 5)):
+        mean_ = sum(k) / len(k)
+        if mean_ <= lower_25:
+            day_grow_dict[i][time_txt_[count]] = "./static/images/tree1.png"
+            grow_list_dict[i].append("./static/images/tree3.png")
+        elif mean_ >= upper_75:
+            day_grow_dict[i][time_txt_[count]] = "./static/images/tree3.png"
+            grow_list_dict[i].append("./static/images/tree1.png")
+        else:
+            day_grow_dict[i][time_txt_[count]] = "./static/images/tree2.png"
+            grow_list_dict[i].append("./static/images/tree2.png")
     peak_dict[i] = []
     for k in graph_dict[i]:
         if not type(k[1]) == int:
@@ -40,13 +66,27 @@ for i in day_list:
             if k[1] >= upper_75:
                 peak_dict[i].append(k[0])
 
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 @app.route("/",methods=["POST","GET"])
 def index():
 
-    # 表示したい時間
-    choose_day = request.form["choose_day"]
-    # choose_day = "day_1"
+    if("choose_day" in request.form):
+        # 表示したい時間
+        choose_day = request.form["choose_day"]
+    else:
+        choose_day = "day_1"
 
     value_list = value_dict[choose_day]
     all_day = all_dict[choose_day]
@@ -54,8 +94,15 @@ def index():
     max_value = max_dict[choose_day]
     min_value = min_dict[choose_day]
     peak_time = peak_dict[choose_day]
+    data_list = day_list
+    graph_list = graph_dict[choose_day]
+    grow_dict  = day_grow_dict[choose_day]
+    grow_list = grow_list_dict[choose_day]
+    print(peak_time)
 
     return render_template("index.html",data_list=data_list,\
                             peak_time=peak_time,value_list=value_list,\
                             all_day=all_day,mean_value=mean_value,\
-                            max_value=max_value,min_value=min_value)
+                            max_value=max_value,min_value=min_value,\
+                            graph_list=graph_list,choose_day=choose_day,grow_dict=grow_dict,\
+                            time_txt_=time_txt_,grow_list=grow_list)
